ca7243af6902042beeaaf8837e20cd3e2f567472
SkyDancer97 <skydance97@gmail.com>
2017-07-05 16:43:45 +0300

    add TableRow class,service, conroller

---
+++ src/main/java/by/specificgroup/kirill/controller/TableRowController.java
@@ -0,0 +1,7 @@
+package by.specificgroup.kirill.controller;
+
+/**
+ * Created by user1 on 7/5/2017.
+ */
+public class TableRowController {
+}
---
+++ src/main/java/by/specificgroup/kirill/service/TableRowService.java
@@ -0,0 +1,7 @@
+package by.specificgroup.kirill.service;
+
+/**
+ * Created by user1 on 7/5/2017.
+ */
+public interface TableRowService {
+}
